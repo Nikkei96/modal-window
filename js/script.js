27ba2dfc064c87be106eb18770743b457b0c2cb3
Nikkei96 <siberiaforce@yandex.ru>
2020-07-10 21:02:45 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener("DOMContentLoaded", function() {
+    "use strict";
+    
+    let more = document.querySelector("button"),
+        overlay = document.querySelector(".overlay"),
+        close = document.querySelector(".popup-close");
+
+        more.addEventListener('click', function(){
+            overlay.style.display = "block";
+            this.classList.add("more-splash");
+            document.body.style.overflow = "hidden";
+        });
+
+        close.addEventListener('click', function() {
+            overlay.style.display = "none";
+            more.classList.remove("more-splash");
+            document.body.style.overflow = "";
+        });
+});
